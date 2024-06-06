1740a1a7b27a225dfc178aad812b8881a8d78988
Andrey Lazarev <git.bastion-tech.ru/a.lazarev>
2024-06-06 20:36:27 +0400

    add save temp logic

--- Assets/Fruitris/Logic/GameModule/GameInstallers/GameplayInstaller.cs
+++ Assets/Fruitris/Logic/GameModule/GameInstallers/GameplayInstaller.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
 
@@ -17,5 +19,54 @@ public class GameplayInstaller : MonoInstaller
         Container.Bind<ScoreView>().FromInstance(_scoreView).AsSingle().NonLazy();
         Container.Bind<LeaderBoard>().FromNew().AsSingle();
         Container.Bind<MergeFruitsStrategy>().FromNew().AsSingle().NonLazy();
-    }    
+    }  
+	
+	const string SaveKey = "localSave";
+
+	private SaveInfo Load()
+	{
+		PlayerPrefs.GetString(SaveKey, "");
+		SaveInfo saveInfo;
+		if (PlayerPrefs.HasKey(SaveKey))
+		{
+			var json = PlayerPrefs.GetString(SaveKey);
+			saveInfo = JsonUtility.FromJson<SaveInfo>(json);
+		}
+		else
+		{
+			saveInfo = new SaveInfo();
+		}
+
+		return saveInfo;
+	}
+
+	private void Save(SaveInfo data)
+	{
+		var json = JsonUtility.ToJson(data);
+		PlayerPrefs.SetString(SaveKey, json);
+	}
+
+	private void Test()
+	{
+		var SaveData = new SaveInfo();
+		SaveData.PlayerData = new List<PlayerData>()
+		{
+			new("123", 123),
+			new("1233R1AS", 123),
+			new("1233R1ASS", 123),
+			new("1233R1AAASDASDS", 123),
+			new("1QWDASVFC233R1AS", 123)
+		};
+
+		var json = JsonUtility.ToJson(SaveData);
+		Debug.Log(json);
+
+		var data = JsonUtility.FromJson<SaveInfo>(json);
+	}
+
+	[Serializable]
+	public class SaveInfo
+	{
+		public List<PlayerData> PlayerData = new();
+	}
 }
--- Assets/Fruitris/Logic/PlayerModule/PlayerData.cs
+++ Assets/Fruitris/Logic/PlayerModule/PlayerData.cs
@@ -3,25 +3,29 @@ using System;
 [Serializable]
 public class PlayerData : IComparable<PlayerData>
 {
-    public string Nickname { get; private set; }
-    public int MaxScore { get; private set; }
+	public string Nickname;
+	public int MaxScore;
 
-    public PlayerData(string nickname, int maxscore)
-    {
-        Nickname = nickname;
-        MaxScore = maxscore;
-    }
+	public PlayerData(string nickname, int maxscore)
+	{
+		Nickname = nickname;
+		MaxScore = maxscore;
+	}
 
-    public PlayerData(Player player)
-    {
-        Nickname = player.Nickname;
-        MaxScore = player.MaxScore;
-    }
+	public PlayerData(Player player)
+	{
+		Nickname = player.Nickname;
+		MaxScore = player.MaxScore;
+	}
 
-    public int CompareTo(PlayerData other)
-    {
-        if (MaxScore < other.MaxScore) return 1;
-        if (MaxScore == other.MaxScore) return 0;
-        return -1;
-    }
+	public int CompareTo(PlayerData other)
+	{
+		if (MaxScore < other.MaxScore)
+			return 1;
+
+		if (MaxScore == other.MaxScore)
+			return 0;
+
+		return -1;
+	}
 }
